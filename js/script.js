1cc8af99a94d7a9f96f84d5c109a6f6153cd7550
Rifah Tasnia <rtasnia007@gmail.com>
2022-09-23 03:21:59 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,119 @@
+//define UI element
+
+let form=document.querySelector('#task_form');
+let taskList=document.querySelector('ul');
+let clearButton=document.querySelector('#clear_task_btn');
+let filter = document.querySelector('#task_filter');
+let taskInput = document.querySelector('#new_task');
+// let addTaskbtn=document.getElementById('add_task');
+
+
+//define function
+
+
+let  addTask=(e)=> {
+    if (taskInput.value === '') {
+        alert('Add a task!');
+    } else {
+        // Create li element
+        // debugger;
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value + " "));
+        let link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.innerHTML = 'x';
+        li.appendChild(link);
+        taskList.appendChild(li);
+
+        storeTaskInLocalStorage(taskInput.value);
+
+        taskInput.value = '';
+
+    }
+    e.preventDefault(); 
+}
+let removeTask=(e)=>{
+    if(e.target.hasAttribute('href')){
+        let ele=e.target.parentElement;
+        ele.remove();
+        removeFromLS(ele);
+    }
+}
+let clearTask=(e)=>{
+// taskList.innerHTML="";
+//faster
+while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+}
+localStorage.clear();
+}
+// Filter Task
+function filterTask(e) {
+    let text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('li').forEach(task => {
+        let item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+}
+// Store in Local Storage
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(task => {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(task + " "));
+        let link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.innerHTML = 'x';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
+}
+
+function removeFromLS(taskItem) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    let li = taskItem;
+    li.removeChild(li.lastChild); // <a>x</a>'
+
+    tasks.forEach((task, index) =>{
+        if(li.textContent.trim() === task) {
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+//define event listener
+form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask)
+clearButton.addEventListener('click', clearTask);
+filter.addEventListener('keyup', filterTask);
+document.addEventListener('DOMContentLoaded',getTasks);
+// addTaskbtn.onclick=addTask;
